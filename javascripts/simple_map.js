9a011e48e8219d6eceec43b8c22b927a48395929
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-02-12 14:03:53 +0200

    Added ability to disable default ui

--- javascripts/simple_map.js
+++ javascripts/simple_map.js
@@ -1,13 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var map;
 
     function initMap() {
-      map = new google.maps.Map($('#map')[0], {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-      });
+        map = new google.maps.Map($('#map')[0], {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
     }
 
     initMap();
+
+    // --------------
+    $('#disable-default-ui').on('change', function () {
+        var checked = $(this).is(':checked');
+        map.setOptions({disableDefaultUI: checked});
+    });
 });
 
