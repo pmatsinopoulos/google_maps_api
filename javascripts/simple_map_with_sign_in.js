15a445805d0f5c01a055a19cb183a7804f3ac282
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-02-12 22:37:13 +0200

    Present a save widget which includes HTML content with extra info

--- javascripts/simple_map_with_sign_in.js
+++ javascripts/simple_map_with_sign_in.js
@@ -2,15 +2,24 @@ $(document).ready(function () {
     (function(){
 
         function initMap() {
-            //37.9715323,23.7235552,16z
+
             var center_location = {lat: 37.9715323, lng: 23.7235552};
             var map = new google.maps.Map($('#map')[0], {
-                zoom: 16,
-                center: center_location
+                zoom: 17,
+                center: center_location,
+                mapTypeControlOptions: {
+                    mapTypeIds: [
+                        google.maps.MapTypeId.ROADMAP,
+                        google.maps.MapTypeId.SATELLITE
+                    ],
+                    position: google.maps.ControlPosition.BOOTOM_LEFT
+                }
             });
 
-            var marker = new google.maps.Marker({
-                map: map,
+            var widgetDiv = $('#save-widget')[0];
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(widgetDiv);
+
+            var saveWidget = new google.maps.SaveWidget(widgetDiv, {
                 place: {
                     location: center_location,
                     query: 'Ακρόπολη, Αθήνα, Ελλάδα'
@@ -19,6 +28,11 @@ $(document).ready(function () {
                     source: 'My Google Map API Demos',
                     webUrl: 'https://www.techcareerbooster.com'
                 }
+            });
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: saveWidget.getPlace().location
             });
 
             var infoWindow = new google.maps.InfoWindow({
